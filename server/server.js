57ef71263bba8501653023b78ae5169587fa8ed0
Jesus Salazar <jase036@gmail.com>
2021-12-03 09:44:00 -0700

    Created app

---
+++ server/server.js
@@ -0,0 +1,32 @@
+"use strict";
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+
+const PORT = 8000;
+const app = express();
+// express()
+
+app.use(function (req, res, next) {
+  res.header(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, HEAD, GET, PUT, POST, DELETE"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+app.use(morgan("tiny"));
+app.use(express.static("./server/assets"));
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
+app.use("/", express.static(__dirname + "/"));
+
+// endpoints
+
+
+app.listen(PORT, () => console.info(`Listening on port ${PORT}`));
